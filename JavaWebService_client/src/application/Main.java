cb94528c50bd601d581e5da24dc44cf54a6df320
DejanJovanovic021 <dejan.jovanovic.java021@gmail.com>
2021-05-05 08:49:45 +0200

    TrancelateWebService client

---
+++ JavaWebService_client/src/application/Main.java
@@ -0,0 +1,12 @@
+
+package application;
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+        TrancelateForm tf = new TrancelateForm();
+        tf.setVisible(true);
+    }
+    
+}
